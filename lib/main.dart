39cce9b8c28e2ed12f26ae86b8dc8cb45001f10b
nazreen <151821397+nazreen-cod@users.noreply.github.com>
2024-05-31 15:23:17 +0800

    Update main.dart
    design

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:flutter/material.dart';
 import 'login_page.dart';
 import 'register_page.dart';
 import 'booking_page_2.dart';
-import 'package:flutter/material.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Homestay Booking',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: HomePage(),
     );
@@ -21,51 +22,91 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Set up constants for repeated values
+    final textStyle = TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.bold,
+      color: Colors.white,
+    );
+    final buttonStyle = ButtonStyle(
+      backgroundColor: MaterialStateProperty.all(Colors.brown),
+      foregroundColor: MaterialStateProperty.all(Colors.white),
+      padding: MaterialStateProperty.all(EdgeInsets.symmetric(horizontal: 16, vertical: 8)),
+    );
+
     return Scaffold(
-      backgroundColor: Colors.blue, // Set background color to blue
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Homestay Booking System',
-              style: TextStyle(
-                fontSize: 32, // Adjust font size to your preference
-                fontWeight: FontWeight.bold, // Apply bold style
-                color: Colors.white, // Set text color to white
+      appBar: AppBar(
+        title: Text('Homestay Booking System'),
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: Image.asset('assets/icon.jpg'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => HomePage()),
+            );
+          },
+        ),
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFF4CAF50),
+              Color(0xFF8BC34A),
+              Color(0xFFCDDC39),
+            ],
+            stops: [0.1, 0.5, 0.9],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              // Display the title of the app
+              Text(
+                'Homestay Booking System',
+                style: textStyle,
+                textAlign: TextAlign.center,
               ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 40), // Add some space between the title and buttons
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => RegisterPage()),
-                );
-              },
-              child: Text('Register'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => LoginPage()),
-                );
-              },
-              child: Text('Login'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate to BookingPage2 regardless of authentication status
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => BookingPage2()),
-                );
-              },
-              child: Text('View Packages'),
-            )
-          ],
+              const SizedBox(height: 40), // Use the const keyword for widgets that don't change
+              ElevatedButton(
+                style: buttonStyle,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => RegisterPage()),
+                  );
+                },
+                child: Text('Register'),
+              ),
+              ElevatedButton(
+                style: buttonStyle,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginPage()),
+                  );
+                },
+                child: Text('Login'),
+              ),
+              ElevatedButton(
+                style: buttonStyle,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => BookingPage2()),
+                  );
+                },
+                child: Text('View Packages'),
+              ),
+            ],
+          ),
         ),
       ),
     );
